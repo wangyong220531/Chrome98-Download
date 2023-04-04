ce0044d06e5f09ea3c0c67d2a576eab0deb37c1f
王勇 <wy19945372694@163.com>
2023-04-04 20:02:23 +0800

    first commit

---
+++ express.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+app.use(express.static(path.resolve("./public")))
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve("./public/index.html"))
+})
+
+app.listen(8787, () => console.log("success"))
